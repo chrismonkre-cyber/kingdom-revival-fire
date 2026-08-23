bcd83154514780ce7b94b63c5e0203ac7e130072
chrismonkre-cyber <chrismonkre@gmail.com>
2026-08-23 19:01:04 -0400

    Change background opacity from 30% to 50%

--- src/components/PageBackground.jsx
+++ src/components/PageBackground.jsx
@@ -3,7 +3,7 @@ export default function PageBackground({ desktopImg, mobileImg }) {
     <div className="fixed inset-0 -z-10">
       <img src={desktopImg} alt="" className="hidden md:block w-full h-full object-cover" />
       <img src={mobileImg} alt="" className="block md:hidden w-full h-full object-cover" />
-      <div className="absolute inset-0 bg-black/30" />
+      <div className="absolute inset-0 bg-black/50" />
     </div>
   );
 }
